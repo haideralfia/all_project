25b1fa1e26687c437ec24077170a9ec985653f93
alfia haider <haideralfia@gmail.com>
2025-11-01 21:37:57 +0530

    Second Commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Counter = () => {
+  return (
+    <>
+      <h1>Counter</h1>
+      <div>
+        <button>Increment</button>
+        <p>Number</p>
+        <button>Decrement</button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
