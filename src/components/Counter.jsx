900511515360d2ae6d8505fbe2d580c8582d190f
alfia haider <haideralfia@gmail.com>
2025-11-01 22:25:45 +0530

    Third Commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
+  const [number, setNumber] = useState(0);
+
+  const increment = () => {
+    setNumber(number + 1);
+  };
+  const decrement = () => {
+    setNumber(number - 1);
+  };
+  const reset = () => {
+    setNumber(0);
+  };
+
+  //fuction to turn count green for positive and red for negative
+  const countColor = () => {
+    if (number > 0) {
+      return 'green';
+    } else if (number < 0) {
+      return 'red';
+    }
+    return 'black';
+  };
+
   return (
     <>
       <h1>Counter</h1>
       <div>
-        <button>Increment</button>
-        <p>Number</p>
-        <button>Decrement</button>
+        <p style={{ color: countColor() }}>{number}</p>
+        <button
+          onClick={() => {
+            increment();
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            decrement();
+          }}
+        >
+          Decrement
+        </button>
+        <button
+          onClick={() => {
+            reset();
+          }}
+        >
+          Reset
+        </button>
       </div>
     </>
   );
